03577c70718c671e92a88650f494cd7358225dad
Gabin Sagot <gabin.sagot@etu.minesparis.psl.eu>
2025-01-31 09:57:09 +0100

    Doc de auto

--- motus.py
+++ motus.py
@@ -7,6 +7,19 @@ from itertools import islice
 from functools import reduce
 from enum import Enum, auto
 
+"""
+auto can be used in place of a value. If used, the Enum machinery will call an Enum's _generate_next_value_() to get an appropriate value. For Enum and IntEnum that appropriate value will be the last value plus one; for Flag and IntFlag it will be the first power-of-two greater than the highest value; for StrEnum it will be the lower-cased version of the member's name. Care must be taken if mixing auto() with manually specified values.
+
+auto instances are only resolved when at the top level of an assignment:
+
+    FIRST = auto() will work (auto() is replaced with 1);
+
+    SECOND = auto(), -2 will work (auto is replaced with 2, so 2, -2 is used to create the SECOND enum member;
+
+    THREE = [auto(), -3] will not work (<auto instance>, -3 is used to create the THREE enum member)
+
+"""
+
 from colorama import Back, Style
 
 # on Enums: see https://docs.python.org/3/howto/enum.html
