db7177d368f0ee5daaa9db083f7a7220903e94c8
Gabin Sagot <gabin.sagot@etu.minesparis.psl.eu>
2025-01-31 11:24:02 +0100

    Première implémentation du jeu

---
+++ main.py
@@ -0,0 +1,30 @@
+import motus as mt
+import sys 
+#On demande à l'utilisateur de rentrer la longueur du mot à trouver
+#On génère un mot de la longueur demandée présent dans le dictionnaire
+
+
+##################### traite le hidden choisi ############
+dico = mt.Dictionary("data/ods6.txt")
+
+if len(sys.argv) > 1:
+    word = sys.argv[1] 
+    if not dico.__contains__(word):
+        raise f"Le mot {word} n'est pas dans le dictionnaire"
+
+if len(sys.argv) < 2:
+    length = int(input("Entrez la longueur du mot à trouver : "))
+    word = dico.sample_of_length(length)
+    
+
+mot_cachee = mt.Hidden(word)
+print(mot_cachee)
+###########################################################
+
+hidden = mt.Hidden(word)
+print(word[0])
+for i in range(10):
+    typed = input("Entrez un mot : ")
+    attempt = hidden.attempt(typed) #On a un objet de type Attempt  
+    print(attempt)
+
